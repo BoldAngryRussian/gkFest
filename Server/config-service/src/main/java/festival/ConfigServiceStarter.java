e103bfa007c6b8ca19d3f34db92e21e4db1ecf66
Alex <caterpillar.alexey@gmail.com>
2019-11-16 16:55:21 +0300

    Реализован облачный сервис конфигурирования

---
+++ Server/config-service/src/main/java/festival/ConfigServiceStarter.java
@@ -0,0 +1,15 @@
+package festival;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServiceStarter {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(ConfigServiceStarter.class,args);
+    }
+}
